bf94e1b4af1d4957ff461ddb3a197daa406e8d34
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-10-22 11:57:29 +0630

    feat: add home screen and write screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,46 @@
 import { Tabs } from "expo-router";
-import React from "react";
 
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 
+import { setTestDeviceIDAsync } from "expo-ads-admob";
+import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
+import Realm from "realm";
+
+const FeelingSchema = {
+  name: "Feeling",
+  properties: {
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primaryKey: "_id",
+};
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const [ready, setReady] = useState(false);
+  const [realm, setRealm] = useState<any>(null);
+  const startLoading = async () => {
+    await setTestDeviceIDAsync("EMULATOR");
+    const connection = await Realm.open({
+      path: "nomadDiaryDB",
+      schema: [FeelingSchema],
+    });
+    setRealm(connection);
+  };
+  const onFinish = () => setReady(true);
+  if (!ready) {
+    return (
+      <AppLoading
+        onError={console.error}
+        startAsync={startLoading}
+        onFinish={onFinish}
+      />
+    );
+  }
 
   return (
     <Tabs
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,105 @@
-import { View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import styled from "styled-components/native";
+// import { useDB } from "../context";
+// import { AdMobBanner } from "expo-ads-admob";
+import colors from "@/colors";
+import { Link } from "expo-router";
+import { FlatList, TouchableOpacity } from "react-native";
 
-export default function HomeScreen() {
-  return <View>Home</View>;
-}
+const View = styled.View`
+  flex: 1;
+  align-items: center;
+  padding: 0px 30px;
+  padding-top: 100px;
+  background-color: ${colors.bgColor};
+`;
+const Title = styled.Text`
+  color: ${colors.textColor};
+  font-size: 38px;
+  font-weight: 500;
+  width: 100%;
+`;
+const Btn = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  height: 80px;
+  width: 80px;
+  border-radius: 40px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.btnColor};
+  elevation: 5;
+  box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
+`;
+
+const Record = styled.View`
+  background-color: ${colors.cardColor};
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 20px;
+  border-radius: 10px;
+  box-shadow: 1px 1px 1px rgba(41, 30, 95, 0.1);
+`;
+const Emotion = styled.Text`
+  font-size: 24px;
+  margin-right: 10px;
+`;
+const Message = styled.Text`
+  font-size: 18px;
+`;
+const Separator = styled.View`
+  height: 10px;
+`;
+
+const Home = () => {
+  // const realm = useDB();
+  const [feelings, setFeelings] = useState([]);
+  // useEffect(() => {
+  //   const feelings = realm.objects("Feeling");
+  //   feelings.addListener((feelings) => {
+  //     LayoutAnimation.spring();
+  //     setFeelings(feelings.sorted("_id", true));
+  //   });
+  //   return () => {
+  //     feelings.removeAllListeners();
+  //   };
+  // }, []);
+  const onPress = (id) => {
+    // realm.write(() => {
+    //   const feeling = realm.objectForPrimaryKey("Feeling", id);
+    //   realm.delete(feeling);
+    // });
+  };
+  return (
+    <View>
+      <Title>My journal</Title>
+      {/* <AdMobBanner
+        bannerSize="fullBanner"
+        adUnitID="ca-app-pub-3940256099942544/2934735716"
+      /> */}
+      <FlatList
+        style={{ marginVertical: 50, width: "100%" }}
+        data={feelings}
+        contentContainerStyle={{ paddingVertical: 10 }}
+        ItemSeparatorComponent={Separator}
+        keyExtractor={(feeling) => feeling._id + ""}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => onPress(item._id)}>
+            <Record>
+              <Emotion>{item.emotion}</Emotion>
+              <Message>{item.message}</Message>
+            </Record>
+          </TouchableOpacity>
+        )}
+      />
+      <Btn>
+        <Link href="/write">
+          <Ionicons name="add" color="white" size={40} />
+        </Link>
+      </Btn>
+    </View>
+  );
+};
+export default Home;
